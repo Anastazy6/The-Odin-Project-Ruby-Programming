33e0ba5a21eea8a19f2a0e9069529de1a7168f86
Anastazy6 <kamil_sidor@o2.pl>
2020-11-12 00:06:12 +0100

    Update caesar.rb
    It is a tiny project i wrote roughly 7 months ago.  I've decided to clean it a little bit with rubocop in order to make it look just a tiny bit less repelling. I've removed non-English variable names and changed indentnation from 8 (don't ask) to 2 spaces. I'm not sure if I should mess with history though. Comparing the old caesar.rb with hangman.rb is nonetheless a way to see some progress.

--- RubyBuildingBlocks/caesar.rb
+++ RubyBuildingBlocks/caesar.rb
@@ -1,50 +1,35 @@
-def kajzer(string, shift)
-        encrypt_this = string.split("")
-        encrypted_stuff = []
-        encrypt_this.each do |char|
-                if char =~ /[^A-Za-z]/
-                        encrypted_stuff.push(char)
-                elsif char =~ /[A-Z]/
-                        lowcased = char.downcase
-                        encrypted_char = encrypt(lowcased, shift)
-                        upcased = encrypted_char.upcase
-                        encrypted_stuff.push(upcased)
-                else
-                        encrypted_char = encrypt(char, shift)
-                        encrypted_stuff.push(encrypted_char)
-                end
-        end
-        result = encrypted_stuff.join()
-        return result
+# frozen_string_literal: true
+
+def caesar(string, shift)
+  encrypt_this = string.split('')
+  encrypted_stuff = []
+  encrypt_this.each do |char|
+    case char
+    when /[^A-Za-z]/ then encrypted_stuff.push(char)
+    when /[A-Z]/
+      lowcased = char.downcase
+      encrypted_char = encrypt(lowcased, shift)
+      upcased = encrypted_char.upcase
+      encrypted_stuff.push(upcased)
+    else
+      encrypted_char = encrypt(char, shift)
+      encrypted_stuff.push(encrypted_char)
+    end
+  end
+  encrypted_stuff.join
 end
 
 def encrypt(letter, shift)
-        alphabeth = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        if letter =~ /[^a-z]/
-                raise "WARNING! A non-downcase letter has been parsed to the encryption machine!"
-        else 
-                id = alphabeth.index(letter)
-                newid = id + shift
-                while newid >= alphabeth.length() do
-                        newid -= alphabeth.length()
-                end
-                while newid < 0 do
-                        newid += alphabeth.length()
-                end
-                return alphabeth[newid]
-        end
-end
+  alphabeth = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 
-def maehinmarwolaethorgloch(&hvafaen)   # Taken from a practice lesson, this method is not a part of this project, I have added it for fun.
-        sztart = Time.now
-    
-        hvafaen.call
-    
-        duration = Time.now - sztart
-    
-        puts "It took " + duration.to_s + " szekondz."
-    end
-    
+  raise 'WARNING! A non-downcase letter has been parsed to the encryption machine!' if letter =~ /[^a-z]/
+
+  id = alphabeth.index(letter)
+  change = id + shift
+  change -= alphabeth.length while change >= alphabeth.length
+  change += alphabeth.length while change.negative?
+  alphabeth[change]
+end
 
 puts "Do you want to secure your message? Here's the notorious for its lack of security, the old, the know, and the easily crackable... CAESAR!"
 puts "Type your message now, you poor, unfortunate soul... >>> \n"
@@ -53,8 +38,6 @@ puts "How large do you want the shift to be? Type an integer... >>> \n"
 shift = gets.chomp
 
 puts "Encrypting started... \n\n"
+puts caesar(message, shift.to_i)
 
-puts "#{maehinmarwolaethorgloch do puts kajzer(message, shift.to_i) end} \n\n"
-
-puts "Encrypting complete."
-
+puts 'Encrypting complete.'
